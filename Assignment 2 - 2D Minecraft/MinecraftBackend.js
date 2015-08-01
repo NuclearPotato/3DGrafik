dcd7017853efc499c1c4a99ab46e9ffdd8e115ea
Lars E. G. Christensen <wukiii@hotmail.com>
2015-08-01 10:19:09 +0200

    SHould work

--- Assignment 2 - 2D Minecraft/MinecraftBackend.js
+++ Assignment 2 - 2D Minecraft/MinecraftBackend.js
@@ -4,13 +4,15 @@ var gl;
 var blockArray = [];
 var vBuffer, cBuffer, lBuffer;
 var index = 0;
-var cIndex;
+var cIndex = 0;
 var worldWidth = 40;
 var worldHeight = 32;
 var groundLevel = worldHeight/2;
 var waterLevel = worldWidth/1.4;
 var mousePosition = [];
 var program;
+var changeBlock = false;
+var chosenBLockType = "Air";
 
 // Uniform variable locations
 var firstCorner, secondCorner, clickPos, waveLength, isAir;
@@ -30,9 +32,9 @@ var colors = [
     vec4(0.8, 0.8, 1.0, 1.0), // Air
     vec4(0.7, 0.5, 0.0, 1.0), // Dirt
     vec4(0.0, 0.8, 0.0, 1.0), // Grass
+    vec4(0.8, 0.0, 0.0, 1.0), // Lava
     vec4(0.5, 0.5, 0.5, 1.0), // Metal
     vec4(0.1, 0.3, 0.8, 1.0), // Water
-    vec4(0.8, 0.0, 0.0, 1.0), // Lava
     vec4(0.0, 0.0, 0.0, 0.0) // Border color
 ];
 
@@ -88,7 +90,8 @@ window.onload = function init() {
 
     //Create eventListener
     m.addEventListener("click", function() {
-       cIndex = m.selectedIndex;
+        cIndex = m.selectedIndex;
+        chosenBLockType = m.value;
         });
 
     //Add eventListener
@@ -96,7 +99,7 @@ window.onload = function init() {
     {
         console.log("x: "+ event.clientX + "  y: " + event.clientY  );
         //gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-        
+        changeBlock = true;
 		var clipPos = pixel_to_clip(event.clientX, event.clientY);
 		
 		waveRadius = 0.0;
@@ -148,6 +151,7 @@ function pixel_to_clip(x,y)
 
 function render()
 {
+    var blockIndex = 0;
     gl.clear( gl.COLOR_BUFFER_BIT );
     for(var i = 0; i<index; i+=4)
     {
@@ -156,8 +160,6 @@ function render()
 		gl.uniform2f(firstCorner,currentBlock.v2[0],currentBlock.v2[1]);
 		gl.uniform2f(secondCorner,currentBlock.v3[0],currentBlock.v3[1]);
 
-        //console.log(i);
-		
 		//Avoid gradient on air blocks.
 		if(currentBlock.blockType == "Air")
 		{
@@ -173,30 +175,29 @@ function render()
         if (mousePosition[0] >= currentBlock.v1[0] && mousePosition[1] >= currentBlock.v1[1]
             && mousePosition[0] < currentBlock.v4[0] && mousePosition[1] < currentBlock.v4[1])
         {
-            //gl.clearColor(0.0, 0.0, 0.0, 1.0);
-            //gl.clear( gl.COLOR_BUFFER_BIT );
+            blockIndex = i/4;
 
-
-
-            //console.log(index);
-            //console.log(blockArray.length*4);
             var tempBlock = new block("Border", currentBlock.v1, currentBlock.v2, currentBlock.v4, currentBlock.v3);
             var tempIndex = index;
             index = blockArray.length*4;
-            allocateToVBuffer(tempBlock);
+            allocateToVBuffer(tempBlock, index);
             index += 4;
             var borderColor = vec4(0.0, 0.0, 0.0, 1.0);
-            allocateToCBuffer(borderColor);
+            allocateToCBuffer(borderColor, index);
             index = tempIndex;
-
-            //gl.drawArrays(gl.LINE_LOOP, i -4, 4);
-            //console.log(block.v1[0] + "   " + block.v1[1]);
-            //console.log(block.v4[0] + "   " + block.v4[1]);
-            //console.log(i);
         }
     }
 
-    gl.drawArrays(gl.LINE_LOOP, blockArray.length*4, 4);
+    if (checkBlockTypesAround(blockIndex)) //Checks if it is possible to build a block
+    {
+        if (changeBlock) {
+            blockArray[blockIndex].blockType = chosenBLockType;
+            var newColor = addColor(chosenBLockType);
+            allocateToCBuffer(newColor, blockIndex*4 + 4);
+        }
+
+        gl.drawArrays(gl.LINE_LOOP, blockArray.length*4, 4);
+    }
     
 //    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
 //    gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(stickmanArray[0]));
@@ -214,10 +215,11 @@ function render()
 	//Handle rippling effect
 	if(waveRadius < 0.5)
 	{
-		waveRadius += 0.02
+		waveRadius += 0.02;
 		gl.uniform1f(waveLength,waveRadius);
 	}
-	
+
+    changeBlock = false;
     window.requestAnimFrame(render);
 }
 
@@ -255,51 +257,47 @@ function initializeCoordSystem(columnSize, rowSize)
     }
 }
 
-//Assign a blockType to a given spot in the blockArray, depending on the specification of the map level.
+//Assign a blockType to a given spot in the blockArray, depending on
+//the specification of the map level.
 function assignBlockType(i,j)
 {
     if (j > groundLevel)
-    {
        return "Air";
-    }
+
     else if (j === groundLevel && i <= waterLevel)
-    {
         return "Grass";
-    }
+
     else if (i > waterLevel)
-    {
         return "Water";
-    }
+
     else
-    {
         return "Dirt";
-    }
 }
 
 function handleBuffer()
 {
     blockArray.forEach(function(entry) {
-        allocateToVBuffer(entry);
+        allocateToVBuffer(entry, index);
         index += 4;
         var blockColor = addColor(entry.blockType); //Assign a color to the block
-        allocateToCBuffer(blockColor);
+        allocateToCBuffer(blockColor, index);
     });
 }
 
-function allocateToVBuffer(entry) {
+function allocateToVBuffer(entry, currentIndex) {
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(entry.v1));
-    gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+1), flatten(entry.v2));
-    gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+2), flatten(entry.v3));
-    gl.bufferSubData(gl.ARRAY_BUFFER, 8*(index+3), flatten(entry.v4));
+    gl.bufferSubData(gl.ARRAY_BUFFER, 8*currentIndex, flatten(entry.v1));
+    gl.bufferSubData(gl.ARRAY_BUFFER, 8*(currentIndex+1), flatten(entry.v2));
+    gl.bufferSubData(gl.ARRAY_BUFFER, 8*(currentIndex+2), flatten(entry.v3));
+    gl.bufferSubData(gl.ARRAY_BUFFER, 8*(currentIndex+3), flatten(entry.v4));
 }
 
-function allocateToCBuffer(color) {
+function allocateToCBuffer(color,currentIndex) {
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-4), flatten(color));
-    gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-3), flatten(color));
-    gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-2), flatten(color));
-    gl.bufferSubData(gl.ARRAY_BUFFER, 16*(index-1), flatten(color));
+    gl.bufferSubData(gl.ARRAY_BUFFER, 16*(currentIndex-4), flatten(color));
+    gl.bufferSubData(gl.ARRAY_BUFFER, 16*(currentIndex-3), flatten(color));
+    gl.bufferSubData(gl.ARRAY_BUFFER, 16*(currentIndex-2), flatten(color));
+    gl.bufferSubData(gl.ARRAY_BUFFER, 16*(currentIndex-1), flatten(color));
 }
 
 //Returns the color of the given blockType
@@ -316,19 +314,63 @@ function addColor(blockType)
         case "Grass" :
             return colors[2];
             break;
-        case "Metal" :
+        case "Lava" :
             return colors[3];
             break;
-		case "Water" :
+		case "Metal" :
             return colors[4];
             break;
-		case "Lava" :
+		case "Water" :
             return colors[5];
             break;
 		case "Border" :
             return colors[6];
             break;
     }
+}
+
+//Returns false if there is only air around the block in the blockArray
+// of the given index.
+function checkBlockTypesAround(blockIndex)
+{
+    var topBool,leftBool,rightBool;
+    var airCounter = 0;
+    var numberOfPossibleAir = 4;
+
+    if (blockIndex % worldHeight == 0 || blockArray[blockIndex].blockType != "Air")
+        return true;
+
+    if (blockIndex % worldHeight == worldHeight-1) {
+        topBool = true;
+        numberOfPossibleAir--;
+    }
+
+    if (blockIndex + worldHeight > blockArray.length) {
+        rightBool = true;
+        numberOfPossibleAir--;
+    }
+
+    if (blockIndex - worldHeight < 0) {
+        leftBool = true;
+        numberOfPossibleAir--;
+    }
+
+    if (blockArray[blockIndex-1].blockType == "Air")
+        airCounter++;
+
+    if (!topBool && blockArray[blockIndex+1].blockType == "Air")
+        airCounter++;
+
+    if (!rightBool && blockArray[blockIndex+worldHeight].blockType == "Air")
+        airCounter++;
+
+    if (!leftBool && blockArray[blockIndex-worldHeight].blockType == "Air")
+        airCounter++;
+
+    if (airCounter == numberOfPossibleAir)
+        return false;
+
+    return true;
 }
 
 
