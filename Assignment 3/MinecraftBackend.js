24de077a4e597e51da399f448d9264b644369e0a
Lars E. G. Christensen <wukiii@hotmail.com>
2015-08-11 21:57:43 +0200

    Update MinecraftBackend.js

--- Assignment 3/MinecraftBackend.js
+++ Assignment 3/MinecraftBackend.js
@@ -760,7 +760,7 @@ function AddEvents()
 			var pos = faceToPos(face, blockPos)
 			var cell = getCell(pos);
 			// Add block at pick location
-			var blockType = sel.value;
+			var blockType = "Solid";
 			addSelectedBlock(cell, blockType, assignBlockAppearance(blockType));
 		}
 		// Change to ortho top-down mapview
